32e473eda39076b81a48fdb1fcd72a4947dd8523
root <root@DESKTOP-TK41HK7.localdomain>
2019-01-09 16:11:51 +0200

    update 9 ian

--- kilo.c
+++ kilo.c
@@ -1,22 +1,50 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+// STDIN_FILENO e din unistd.h
+
 struct termios orig_termios;
 
 void disableRawMode() {
-	tsetattr(STDIN_FILENO, TCSAFLUSH, &orig_terios);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode() {
-	struct termios raw;
-	tcgetattr(STDIN_FILENO, &raw);
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);// IXON = soft. flow control, ICRNL = input Carriage Return, New Line. 
+  // BRKINT = break condition.
+  // INPCK = parity check 
+  // ISTRIP = causes the 8th bit to be strippepd
+  // CS8 = not a flag, a bit maask. sets char size to 8 bits per byte.
+ 
+  raw.c_oflag &= ~(OPOST);// Output Post-Processing.
+  raw.c_cflag |= (CS8);
+  raw.co_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);// astia sunt toti flags
+  raw.c_cc[VMIN] =  0;
+  raw.c_cc[VTIME] =  1;
+
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
-	enableRawMode();	
-	char c;
-	while(read(STDIN_FILENO, &c, 1) == 1 && c!='q'); //test comment
+	enableRawMode();
+	
+	char c = '\0';
+	// citeste un beat de data in variabila c, paana cand EOF sau 'q' pressed
+  // read() e din unistd.h
+  read(STDOM_FILENO, &c, 1);
+  if (iscntfl(c)) {
+          printf("%d\r\n", c);
+  } else {
+          printf("%d ('%c')\r\n", c, c);
+  }
+  if (c == 'q') break;
+
 	return 0;
 }
